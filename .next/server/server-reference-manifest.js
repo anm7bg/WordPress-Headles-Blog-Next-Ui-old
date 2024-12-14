f5f6f395c4723947665e0e3abd3c30ccb01e7ab7
anm7 <nsite2014@gmail.com>
2024-12-15 00:14:50 +0200

    update

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Qcx1BKfKucuo7ya85e8lKPwJgW7twSfbnXNq4o3mJ/A=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
